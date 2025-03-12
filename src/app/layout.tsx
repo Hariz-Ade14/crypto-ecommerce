a7afca57a919f84a2d92b1dffb4c12b165a3c98c
Harizz_TheGreat <harizadebayo8@gmail.com>
2025-03-11 18:01:46 -0700

    update Next.js version to use caret range and replace font import with Josefin Slab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Josefin_Slab,Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const josefinSlab = Josefin_Slab({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
+  
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -15,6 +15,14 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Portfolio",
   description: "Pertfolio page",
+  icons: {
+    icon: [
+      {
+        rel: 'icon',
+        url: '/images/portfolio-pic.jpg'
+      }
+    ]
+  }
 };
 
 export default function RootLayout({
@@ -25,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${josefinSlab.className} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
